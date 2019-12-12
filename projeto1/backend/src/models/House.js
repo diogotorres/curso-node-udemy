f45ec985098cb7657aaa28e06fe00548f40a8070
Diogo Torres <diogo.torres93@gmail.com>
2019-12-11 23:03:55 -0300

    crud de houses

---
+++ projeto1/backend/src/models/House.js
@@ -0,0 +1,25 @@
+import { Schema, model } from 'mongoose';
+
+const HouseSchema = new Schema({
+  thumbnail: String,
+  description: String,
+  price: Number,
+  location: String,
+  status: Boolean,
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  }
+},
+{
+  toJSON: {
+    virtuals: true
+  }
+});
+
+//campo nao persistido, utilizado apenas para retornar no JSON
+HouseSchema.virtual('thumbnail_url').get(function() {
+  return `http://localhost:3333/files/${this.thumbnail}`;
+})
+
+export default model('House', HouseSchema);
