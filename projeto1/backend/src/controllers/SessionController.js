05a0d466a3bb682ad087df93edd9e115fa90672c
Diogo Torres <diogo.torres93@gmail.com>
2019-12-12 20:52:43 -0300

    validacoes com Yup

--- projeto1/backend/src/controllers/SessionController.js
+++ projeto1/backend/src/controllers/SessionController.js
@@ -1,8 +1,18 @@
 import User from '../models/User';
+import * as Yup from 'yup';
 
 class SessionController {
+
   async store(req, res) {
+    const schema = Yup.object().shape({
+      email: Yup.string().email().required()
+    });
+
     const email = req.body.email;
+
+    if(!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Falha na validação.' });
+    }
 
     //verifica se o usuario ja nao existe
     let user = await User.findOne({ email: email });
@@ -12,6 +22,7 @@ class SessionController {
 
     return res.json({user});
   }
+
 }
 
 export default new SessionController();
