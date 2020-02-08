3b0aad094959a2b1fa63ce38dde95ed3edc8c8a0
Diogo Torres <diogo.torres93@gmail.com>
2020-02-08 15:04:36 -0300

    projeto 2 iniciado

---
+++ projeto2/src/app/controllers/UserController.js
@@ -0,0 +1,24 @@
+import User from '../models/User';
+
+class UserController {
+  async store(req, res) {
+
+    const persisted = await User.findOne({
+      where: { email: req.body.email }
+    });
+
+    if(persisted) {
+      return res.status(400).json({ error: 'Usuario ja existe' });
+    }
+
+    const { id, name, email } = await User.create(req.body);
+
+    return res.json({
+      id,
+      email,
+      name
+    })
+  }
+}
+
+export default new UserController();
